fed93947c96cc1b6ddaf17d285f365ac2782246b
Victor <victorveracoronel@outlook.es>
2023-07-27 16:49:38 +0200

    He retocado el estilo de las tarjetas de usuario, de los botones y de los links de navegacion. (A grosso modo, he cambiado los colores).

--- crud_usuarios_frontend/src/components/UserCard.jsx
+++ crud_usuarios_frontend/src/components/UserCard.jsx
@@ -4,20 +4,21 @@ import "./styles/UserCard.css";
 export function UserCard({ user }) {
   const navigate = useNavigate();
 
-  return (
-    <div className="usercard" onClick={() => {
-      navigate('/users/'+user.id)
-    }}>
-      <div className="identification">
-        <h1 id="name">{user.name}</h1>
-        <h1 id="surname">{user.surname}</h1>
-      </div>
+  //<div className="info">
+  //  <h2>{user.email}</h2>
+  //  <h2>{user.phone}</h2>
+  //  <h2>{user.birth_date}</h2>
+  //</div>;
 
-      <div className="info">
-        <h2>{user.email}</h2>
-        <h2>{user.phone}</h2>
-        <h2>{user.birth_date}</h2>
-      </div>
+  return (
+    <div
+      className="usercard"
+      onClick={() => {
+        navigate("/users/" + user.id);
+      }}
+    >
+      <h1 id="name">{user.name}</h1>
+      <h1 id="surname">{user.surname}</h1>
     </div>
   );
 }
